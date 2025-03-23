0962aec4c5a2f26a2dfa78f89442217606c7ad11
Abel Teixeira <120480217+ttabelhaxd@users.noreply.github.com>
2025-03-23 16:11:00 +0000

    added dependency for room database

--- SnapQuest/app/build.gradle.kts
+++ SnapQuest/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -59,5 +60,10 @@ dependencies {
 
     //dependency for the navigation.
     implementation(libs.androidx.navigation.compose)
+
+    //dependency for room database
+    implementation(libs.androidx.room.runtime)
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+
 
 }
